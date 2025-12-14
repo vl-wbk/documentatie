debd72faf807bc78b8d4b803aeba5486f583ff01
Tjoosten <Topairy@gmail.com>
2025-12-14 05:17:52 +0100

    meh

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-    output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+    //
 })
 
 export default withNextra({
     output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 })
